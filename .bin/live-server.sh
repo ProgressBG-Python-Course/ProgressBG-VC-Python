bc981701b659e2fd264fa6333eaf1b00f45a9b14
ProgressBG-Python-Course <ProgressBG.Python.Course@gmail.com>
2018-03-19 01:08:18 +0200

    Lab1: 19.03.2018 DONE: Python instal (2,3) NEXT: pipenv, next TODO: More examples on Windows with clear system

---
+++ .bin/live-server.sh
@@ -0,0 +1,5 @@
+nohup live-server --port=3333 --open=/ProgressBG-VC-Python/pages/agenda/agenda.html \
+--watch=/ProgressBG-VC-Python   \
+/home/nemsys/projects/www/wwwcourses.github.io &
+
+echo 'live-server started on port=3333 for ProgressBG-VC-Python'
