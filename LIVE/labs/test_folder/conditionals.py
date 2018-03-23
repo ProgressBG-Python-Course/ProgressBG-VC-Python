6d12d7a24ade80528b6770fd5fbc2a87a77dc270
ProgressBG-Python-Course <ProgressBG.Python.Course@gmail.com>
2018-03-23 13:11:58 +0200

    Lab3: 23.03.2017 DONE: Conditionals NEXT: Loops TODO: Check .TODO

---
+++ LIVE/labs/test_folder/conditionals.py
@@ -0,0 +1,9 @@
+x = 40
+
+if x > 18:
+    print("block 1")
+    pass
+    print("jdsfdskfds")
+else:
+    passx
+
---
+++ LIVE/labs/test_folder/lab_to_astro.py
@@ -0,0 +1,3 @@
+user_name = "ada"
+str = "{1:10s} - {}".format("hello", user_name)
+print(str)
