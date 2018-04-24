c96af77cc70e108aa3e555d2c00df8f16e281ba3
ProgressBG-Python-Course <ProgressBG.Python.Course@gmail.com>
2018-04-24 10:11:01 +0300

    Debug UP

--- pages/themes/beginners/debug/examples/debug_demo.py
+++ pages/themes/beginners/debug/examples/debug_demo.py
@@ -1,5 +1,13 @@
-x = 1
+def sum_numbers(num_range):
+  suma = 0
+  for i in num_range:
+    suma =+ i
+    import pdb; pdb.set_trace()
 
-for i in range(3):
-    x+=i
-print(x)
+  return suma
+
+def main():
+  print( sum_numbers(range(1,10)) )
+
+if __name__ == '__main__':
+  main()
