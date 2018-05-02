2c25ea681a8a2f06501a6a27514c67fa5d47df86
ProgressBG-Python-Course <ProgressBG.Python.Course@gmail.com>
2018-05-02 13:10:20 +0300

    Lab16: 02.05.2018 DONE: pandas NEXT: Async, mult; SQL

---
+++ LIVE/labs/pandas/DF/create_DF.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+prices_ds = pd.Series([1.5, 2, 2.5, 3],dtype=int8)
+print(prices_ds)
---
+++ LIVE/labs/pandas/DF/read_csv.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+titles_df=pd.read_csv("../../../../pages/themes/beginners/dataScienceBasics/examples/pandas/read_csv/title.basics_sample_500.tsv", sep="\t")
+print(titles_df)
